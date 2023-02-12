50749cd14dd1bce1489b84450364be9fd1ab1899
sp <sp@DESKTOP-DTROK37>
2023-02-12 20:11:14 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+            do
+            {
+                Console.WriteLine("Ingrese el primer número");
+                n1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese el segundo número");
+                n2 = int.Parse(Console.ReadLine());
+                if (n1 == n2)
+                {
+                    Console.WriteLine("Los números ingresados no deben ser iguales");
+                }
+
+            } while (n1 == n2);
+            if (n1 > n2)
+            {
+                Console.WriteLine($"El número {n1} es el numero mayor");
+                Console.WriteLine($"El número {n2} es el numero menor");
+            }
+            else
+            {
+                Console.WriteLine($"El número {n2} es el numero mayor");
+                Console.WriteLine($"El número {n1} es el numero menor");
+            }
+            Console.ReadLine();
+        }
+    }
+}
